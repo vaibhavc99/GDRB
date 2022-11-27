0a10694ee1e7389c3ce14fe159c6ea7df86022f4
vaibhavc99 <vaibhavchaudhari676@gmail.com>
2022-11-27 17:01:16 +0100

    Fixes in TCP interface

--- SourceCode-GFC/src/main/java/edu/wsu/eecs/gfc/exps/GFC.java
+++ SourceCode-GFC/src/main/java/edu/wsu/eecs/gfc/exps/GFC.java
@@ -4,6 +4,7 @@ import edu.wsu.eecs.gfc.core.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,8 +18,13 @@ import org.json.JSONObject;
  */
 public class GFC {
 
-    
     private static final Logger log = LoggerFactory.getLogger(GFC.class);
+    private static TrainGFC training = new TrainGFC();
+    private static TestGFC testing = new TestGFC();
+
+    // public GFC() {
+    //     training = new TrainGFC();
+    // }
 
     public static void listen(int port,String inputDir,String outputDir,double minSupp,double minConf,int maxSize,int topK) throws Exception {
 
@@ -27,82 +33,77 @@ public class GFC {
             Socket client = serverSocket.accept();
             log.info("Accepted connection");
 
-            while(true) {
-                try{
+            try{
+                InputStream input = client.getInputStream();
+                DataOutputStream outputStream  = new DataOutputStream(client.getOutputStream());
+                DataInputStream dis = new DataInputStream(input);
+                byte[] buffer = new byte[1024];
+                while(true) {
                     log.info("Waiting for a request");
-                    InputStream input = client.getInputStream();
-                    DataOutputStream outputStream  = new DataOutputStream(client.getOutputStream());
-                    BufferedReader br = new BufferedReader(new InputStreamReader(input));
-                    String request = br.readLine();
-                    if(request == ""){
+                    dis.read(buffer);
+                    String request = new String(buffer, StandardCharsets.UTF_8).trim();
+
+                    if(request == null){
                         log.info("Connection closed");
                         client.close();
+                        dis.close();
                         return;
                     }
-                    log.info("Request received");
-    
-                    JSONObject requestJSON = new JSONObject(request.toString());
-    
-                    if ((requestJSON.getString("type") == "call") && (requestJSON.getString("content") == "type")) {
-    
+                    log.info("Request received: " + request);
+
+                    JSONObject requestJSON = new JSONObject(request);
+
+                    if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("type"))) {
+
                         requestJSON.put("type","type_response");
                         requestJSON.put("content","supervised");
-                        outputStream.writeUTF(requestJSON.toString());
-    
+                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8)); 
                     }
-    
-                    if ((requestJSON.getString("type") == "call") && (requestJSON.getString("content") == "training_start")) {
-    
+
+                    if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("training_start"))) {
+
                         requestJSON.put("type","ack");
                         requestJSON.put("content","training_start_ack");
-                        outputStream.writeUTF(requestJSON.toString());
-    
+                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8));
                     }
-    
-                    if ((requestJSON.getString("type") == "train")) {
-    
+
+                    if ((requestJSON.getString("type").equals("train"))) {
+
+                        training.addTrainingData(requestJSON.getString("subject"),requestJSON.getString("predicate"),requestJSON.getString("object"),requestJSON.getString("score"));
                         requestJSON.put("type","ack");
-                        TrainGFC.addTrainingData(requestJSON.getString("subject"),requestJSON.getString("predicate"),
-                                                    requestJSON.getString("object"),requestJSON.getString("score"));
                         requestJSON.put("content","train_ack");
-                        outputStream.writeUTF(requestJSON.toString());
-    
+                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8));
+                        log.info("Request answered");
+                        continue;
                     }
-    
-                    if ((requestJSON.getString("type") == "call") && (requestJSON.getString("content") == "training_complete")) {
-    
-                        TrainGFC.train(inputDir,outputDir,minSupp,minConf,maxSize,topK);
+                    
+                    if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("training_complete"))) {
+                        training.train(inputDir,outputDir,minSupp,minConf,maxSize,topK);
                         requestJSON.put("type","ack");
                         requestJSON.put("content","training_complete_ack");
-                        outputStream.writeUTF(requestJSON.toString());
-    
+                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8)); 
                     }
-    
-                    if ((requestJSON.getString("type") == "test")) {
-    
-                        TestGFC.addTestingData(requestJSON.getString("subject"),requestJSON.getString("predicate"),
-                                                    requestJSON.getString("object"));
-                        TestGFC.test(inputDir,outputDir,topK);
+
+                    if ((requestJSON.getString("type").equals("test"))) {
+                        // testing.addTestingData(requestJSON.getString("subject"),requestJSON.getString("predicate"),requestJSON.getString("object"));
+                        testing.test(training.getMiner(),training.getModels(),inputDir,outputDir);
                         requestJSON.put("type","test_result");
                         //TODO: Individual assertion score
                         // requestJSON.put("score",prediction);
-                        outputStream.writeUTF(requestJSON.toString());
-    
+                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8));
+                        break;
                     }
-    
                     log.info("Request answered");
-    
                 }
-                catch(IOException | JSONException e){
-                    e.printStackTrace();
-                }
+            }        
+            catch(IOException | JSONException e){
+                e.printStackTrace();
             }
-            
+            client.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public static void main(String[] args) throws Exception {
@@ -116,6 +117,8 @@ public class GFC {
         int topK = Integer.parseInt(args[5]);
 
         listen(4011,inputDir,outputDir,minSupp,minConf,maxSize,topK);
-
+        // TrainGFC training = new TrainGFC();
+        // training.train(inputDir,outputDir,minSupp,minConf,maxSize,topK);
+        // TestGFC.test(training.getMiner(),training.getModels(),inputDir,outputDir);
     }
 }
