2a14239bba1c497aa6757ddb097314439dadb159
vaibhavc99 <vaibhavchaudhari676@gmail.com>
2022-12-11 15:48:20 +0100

    Updated to return results in hashmap

--- SourceCode-GFC/src/main/java/edu/wsu/eecs/gfc/core/TestGFC.java
+++ SourceCode-GFC/src/main/java/edu/wsu/eecs/gfc/core/TestGFC.java
@@ -10,13 +10,14 @@ public class TestGFC {
 
     public List<String> testingAssertions = new ArrayList<>();
     public List<JSONObject> jsonList = new ArrayList<>();
+    public LinkedHashMap<String,String> results = new  LinkedHashMap<>();
 
     private static ParseTriple parse = new ParseTriple();
 
-    public List<JSONObject> test(RuleMiner<String, String> miner,Map<String, Classifier> Models,String inputDir,String outputPath) 
+    public LinkedHashMap<String,String> test(RuleMiner<String, String> miner,Map<String, Classifier> Models,String inputDir,String outputPath) 
                             throws Exception {
 
-        List<Relation<String, String>> relationList = IO.loadRelations(inputDir);
+        List<Relation<String, String>> relationList = IO.loadRelations(inputDir,this.testingAssertions);
         
         for (Relation<String, String> r : relationList) {
         // System.out.println("FactChecker: OFact_R: "
@@ -29,18 +30,25 @@ public class TestGFC {
 
             List<OGFCRule<String, String>> patternsTest = miner.OGFC_stream(r, sampler.getDataTest().get(true), sampler.getDataTest().get(false));
 
+            // if(patternsTest.size() < 1){
+            //     continue;
+            // }
+
             LinkedHashMap<String, String> predictionsR = FactChecker.Test_LRModel(r, patternsTest, Models,sampler.getDataTest(), outputPath);
             
             for(int i=0; i<testingAssertions.size(); i++){
                 for(Map.Entry<String,String> e:predictionsR.entrySet()){
                     if(testingAssertions.get(i).contains(e.getKey())){
-                        this.jsonList.get(i).put("score",e.getValue());
+                        // this.jsonList.get(i).put("score",e.getValue());
+                        JSONObject AssertionJson = this.jsonList.get(i);
+                        String assertion = AssertionJson.getString("subject") + " " + AssertionJson.getString("predicate") + " " + AssertionJson.getString("object");
+                        String score = e.getValue();
+                        this.results.put(assertion,score);
                     }
                 }                
             }
         }
-        return this.jsonList;
-        
+        return this.results;
    }
 
    public void addTestingData(String sub, String pred, String obj, String truthVal) throws JSONException{
@@ -48,7 +56,7 @@ public class TestGFC {
     JSONObject rdf = new JSONObject();
     rdf.put("subject",sub);rdf.put("predicate",pred);rdf.put("object",obj);rdf.put("score",truthVal);
     this.jsonList.add(rdf);
-    // String assertion = sub +" " + pred +" " + obj +" ";
+
     PreProcessedTriple preprocessedTriple = parse.preprocessTriple(sub,obj,pred,truthVal);
     String parsedAssertion = parse.parseTriples(preprocessedTriple.subject(),preprocessedTriple.object(),preprocessedTriple.predicate(),preprocessedTriple.truthValue());
     this.testingAssertions.add(parsedAssertion);
