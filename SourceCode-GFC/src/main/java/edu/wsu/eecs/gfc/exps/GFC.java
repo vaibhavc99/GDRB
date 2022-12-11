08eed6221648de1f080c0b4f18efcfd22440c7ef
vaibhavc99 <vaibhavchaudhari676@gmail.com>
2022-12-11 15:52:34 +0100

    Modified TCP-interface to sync changes in FaVEL

--- SourceCode-GFC/src/main/java/edu/wsu/eecs/gfc/exps/GFC.java
+++ SourceCode-GFC/src/main/java/edu/wsu/eecs/gfc/exps/GFC.java
@@ -22,95 +22,113 @@ public class GFC {
     private static final Logger log = LoggerFactory.getLogger(GFC.class);
     private static TrainGFC training = new TrainGFC();
     private static TestGFC testing = new TestGFC();
-
-    // public GFC() {
-    //     training = new TrainGFC();
-    // }
+    public static LinkedHashMap<String,String> results = new  LinkedHashMap<>();
 
     public static void listen(int port,String inputDir,String outputDir,double minSupp,double minConf,int maxSize,int topK) throws Exception {
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            log.info("Waiting for connection on port ",port);
-            Socket client = serverSocket.accept();
-            log.info("Accepted connection");
+            while(true) {
+                log.info("Waiting for connection on port ",port);
+                Socket client = serverSocket.accept();
+                log.info("Accepted connection");
 
-            try{
-                InputStream input = client.getInputStream();
-                DataOutputStream outputStream  = new DataOutputStream(client.getOutputStream());
-                DataInputStream dis = new DataInputStream(input);
-                byte[] buffer = new byte[1024];
-                while(true) {
-                    log.info("Waiting for a request");
-                    dis.read(buffer);
-                    String request = new String(buffer, StandardCharsets.UTF_8).trim();
+                try{
+                    InputStream input = client.getInputStream();
+                    DataOutputStream outputStream  = new DataOutputStream(client.getOutputStream());
+                    DataInputStream dis = new DataInputStream(input);
+                    byte[] buffer = new byte[1024];
+                    while(true) {
+                        log.info("Waiting for a request");
+                        dis.read(buffer);
+                        String request = new String(buffer, StandardCharsets.UTF_8).trim();
 
-                    if(request == null){
-                        log.info("Connection closed");
-                        client.close();
-                        dis.close();
-                        return;
-                    }
-                    log.info("Request received: " + request);
+                        if(request.equals(null)){
+                            log.info("Connection closed");
+                            client.close();
+                            dis.close();
+                            return;
+                        }
+                        log.info("Request received: " + request);
 
-                    JSONObject requestJSON = new JSONObject(request);
+                        JSONObject requestJSON = new JSONObject(request);
 
-                    if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("type"))) {
+                        if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("type"))) {
+                            JSONObject response = new JSONObject();
+                            response.put("type","type_response");
+                            response.put("content","supervised");
+                            outputStream.write(response.toString().getBytes(StandardCharsets.UTF_8)); 
+                        }
 
-                        requestJSON.put("type","type_response");
-                        requestJSON.put("content","supervised");
-                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8)); 
-                    }
+                        if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("training_start"))) {
+                            JSONObject response = new JSONObject();
+                            response.put("type","ack");
+                            response.put("content","training_start_ack");
+                            outputStream.write(response.toString().getBytes(StandardCharsets.UTF_8));
+                        }
 
-                    if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("training_start"))) {
+                        if ((requestJSON.getString("type").equals("train"))) {
+                            training.addTrainingData(requestJSON.getString("subject"),requestJSON.getString("predicate"),requestJSON.getString("object"),requestJSON.getString("score"));
+                            JSONObject response = new JSONObject();
+                            response.put("type","ack");
+                            response.put("content","train_ack");
+                            outputStream.write(response.toString().getBytes(StandardCharsets.UTF_8));
+                            log.info("Request answered");
+                            continue;
+                        }
+                        
+                        if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("training_complete"))) {
+                            training.train(inputDir,outputDir,minSupp,minConf,maxSize,topK);
+                            JSONObject response = new JSONObject();
+                            response.put("type","ack");
+                            response.put("content","training_complete_ack");
+                            outputStream.write(response.toString().getBytes(StandardCharsets.UTF_8)); 
+                        }
 
-                        requestJSON.put("type","ack");
-                        requestJSON.put("content","training_start_ack");
-                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8));
-                    }
+                        if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("testing_start"))) {
+                            JSONObject response = new JSONObject();
+                            response.put("type","ack");
+                            response.put("content","testing_start_ack");
+                            outputStream.write(response.toString().getBytes(StandardCharsets.UTF_8));
+                        }
 
-                    if ((requestJSON.getString("type").equals("train"))) {
-                        training.addTrainingData(requestJSON.getString("subject"),requestJSON.getString("predicate"),requestJSON.getString("object"),requestJSON.getString("score"));
-                        requestJSON.put("type","ack");
-                        requestJSON.put("content","train_ack");
-                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8));
-                        log.info("Request answered");
-                        continue;
-                    }
-                    
-                    if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("training_complete"))) {
-                        training.train(inputDir,outputDir,minSupp,minConf,maxSize,topK);
-                        requestJSON.put("type","ack");
-                        requestJSON.put("content","training_complete_ack");
-                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8)); 
-                    }
+                        if ((requestJSON.getString("type").equals("testGFC"))) {
+                            testing.addTestingData(requestJSON.getString("subject"),requestJSON.getString("predicate"),requestJSON.getString("object"),requestJSON.getString("score"));
+                            JSONObject response = new JSONObject();
+                            response.put("type","ack");
+                            response.put("content","testGFC_ack");
+                            outputStream.write(response.toString().getBytes(StandardCharsets.UTF_8));
+                            log.info("Request answered");
+                            continue;
+                        }
 
-                    if ((requestJSON.getString("type").equals("test"))) {
-                        testing.addTestingData(requestJSON.getString("subject"),requestJSON.getString("predicate"),requestJSON.getString("object"),requestJSON.getString("score"));
-                        requestJSON.put("type","ack");
-                        requestJSON.put("content","test_ack");
-                        outputStream.write(requestJSON.toString().getBytes(StandardCharsets.UTF_8));
-                        log.info("Request answered");
-                        continue;
-                    }
+                        if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("testing_complete"))) {
+                            results = testing.test(training.getMiner(),training.getModels(),inputDir,outputDir);
+                            JSONObject response = new JSONObject();
+                            response.put("type","ack");
+                            response.put("content","testing_complete_ack");
+                            outputStream.write(response.toString().getBytes(StandardCharsets.UTF_8));
+                            log.info("Request answered");
+                        }
 
-                    if ((requestJSON.getString("type").equals("call")) && (requestJSON.getString("content").equals("testing_complete"))) {
-                        List<JSONObject> results = testing.test(training.getMiner(),training.getModels(),inputDir,outputDir);
-                        // requestJSON.put("type","test_result");
-                        for(JSONObject result:results){
-                            result.put("type","test_result");
-                            outputStream.write(result.toString().getBytes(StandardCharsets.UTF_8));
+                        if ((requestJSON.getString("type").equals("test"))) {
+                            String requestAssertion = requestJSON.getString("subject") + " " + requestJSON.getString("predicate") + " " +
+                                                    requestJSON.getString("object");
+                            String score = results.get(requestAssertion);
+                            JSONObject response = new JSONObject();
+                            response.put("type","test_result");
+                            response.put("score",score);
+                            outputStream.write(response.toString().getBytes(StandardCharsets.UTF_8));
+                            log.info("Request answered");
+                            continue;
                         }
                         log.info("Request answered");
-                        break;
                     }
-                    log.info("Request answered");
+                }        
+                catch(IOException | JSONException e){
+                    e.printStackTrace();
                 }
-            }        
-            catch(IOException | JSONException e){
-                e.printStackTrace();
+                client.close();
             }
-            client.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
